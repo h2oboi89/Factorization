78558f410aea7241b097f78c7e36a869a2d52fe1
h2oboi89 <h2oboi89@gmail.com>
2014-11-24 00:47:25 -0500

    removing main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import factorization
 
 def main():
-    primes = factorization.primes(10)
     
-    for p in factorization.primes(20, 10, primes):
-        print(str(p), end=' ')
-    print()
 
 if __name__ == '__main__':
     main()
