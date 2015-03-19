c70ec35ce31beca37c1d2b2e6c74301f5f7f5bbb
h2oboi89 <h2oboi89@gmail.com>
2015-03-19 00:24:50 -0400

    added "#! python3" to begin of file

--- factorization.py
+++ factorization.py
@@ -1,3 +1,5 @@
+#! python3
+
 import math
 
 _INIT_START = 3
@@ -7,82 +9,82 @@ _PRIMES = []
 def primes(n):
     """ Returns  a list of primes <= n """
     global _START, _PRIMES
-    
+
     if n > _INIT_START and _START >= n:
         primes = []
-        
+
         for p in _PRIMES:
             if p <= n:
                 primes.append(p)
             else:
                 break
-                
+
         return primes
     else:
         _PRIMES = _primes(n + 1, _START, _PRIMES)
         _START = n
-    
+
         return _PRIMES
-    
+
 def _primes(n, start, primes):
     if len(primes) > 0:
         sieve = [False] * start
-    
+
         while len(sieve) < n:
             sieve.append(True)
-        
+
         for p in primes:
             if p < n:
                 sieve[p] = True
     else:
         sieve = [True] * n
-        
+
     limit = math.ceil(math.sqrt(n))
-    
-    for i in range(_INIT_START, limit, 2):        
+
+    for i in range(_INIT_START, limit, 2):
         if sieve[i]:
             sieve[i * i :: 2 * i] = [False] * int((n - i * i - 1) / (2 * i) + 1)
-    
+
     return [2] + [i for i in range(_INIT_START, n, 2) if sieve[i]]
 
 def is_prime(n):
     """ Returns true if n is prime; otherwise false """
     if n < 2:
         return False
-    
+
     for p in primes(n):
         if n == p:
             return True
-    
+
     return False
-    
+
 def factor(n):
     """ Returns list of prime factors of a integer """
     factors = []
-    
+
     if not isinstance(n, int):
         return factors
-                 
+
     n = abs(n)
-    
+
     if n == 0 or n == 1:
         factors.append(n)
     else:
         p_n = primes(math.ceil(math.sqrt(n)))
-        
+
         for p in p_n:
             while True:
                 if n % p == 0:
                     factors.append(p)
-                    
+
                     n = n // p
                 else:
                     break
-            
+
             if p > n:
                 break
-                
+
         if len(factors) == 0:
             factors.append(n)
-    
+
     return factors
