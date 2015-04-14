d9c86c5bc2da4b35f138a310462edfe69f7bfe65
Daniel Waters <h2oboi89@gmail.com>
2015-04-14 16:24:32 -0400

    changed prime generation to take 2nd optional argument of primes that have already been generated

--- factorization.py
+++ factorization.py
@@ -2,63 +2,55 @@
 
 import math
 
-_INIT_START = 3
-_START = _INIT_START
-_PRIMES = []
-
-def primes(n):
+def primes(n, prime_numbers = []):
     """ Returns  a list of primes <= n """
-    global _START, _PRIMES
-
-    if n > _INIT_START and _START >= n:
-        primes = []
-
-        for p in _PRIMES:
-            if p <= n:
-                primes.append(p)
-            else:
-                break
-
-        return primes
+    if len(prime_numbers) > 0:
+        if prime_numbers[-1] >= n:
+            return [p for p in prime_numbers if p <= n]
     else:
-        _PRIMES = _primes(n + 1, _START, _PRIMES)
-        _START = n
+        return _generate_primes(n + 1, prime_numbers)
 
-        return _PRIMES
+def _limit(n):
+    return int(math.ceil(math.sqrt(n)))
 
-def _primes(n, start, primes):
-    if len(primes) > 0:
-        sieve = [False] * start
+def _generate_primes(n, prime_numbers = []):
+    start = 3
+
+    if len(prime_numbers) > 0:
+        sieve = [False] * prime_numbers[-1]
+
+        start = len(sieve)
 
         while len(sieve) < n:
             sieve.append(True)
 
-        for p in primes:
+        for p in prime_numbers:
             if p < n:
                 sieve[p] = True
     else:
         sieve = [True] * n
 
-    limit = math.ceil(math.sqrt(n))
-
-    for i in range(_INIT_START, limit, 2):
+    for i in range(start, _limit(n), 2):
         if sieve[i]:
             sieve[i * i :: 2 * i] = [False] * int((n - i * i - 1) / (2 * i) + 1)
 
-    return [2] + [i for i in range(_INIT_START, n, 2) if sieve[i]]
+    return [2] + [i for i in range(3, n, 2) if sieve[i]]
 
-def is_prime(n):
+def is_prime(n, prime_numbers = []):
     """ Returns true if n is prime; otherwise false """
     if n < 2:
         return False
 
-    for p in primes(n):
+    if len(prime_numbers) == 0 or prime_numbers[-1] < n:
+        prime_numbers = _generate_primes(n + 1, prime_numbers)
+
+    for p in prime_numbers:
         if n == p:
             return True
 
     return False
 
-def factor(n):
+def factor(n, prime_numbers = []):
     """ Returns list of prime factors of a integer """
     factors = []
 
@@ -70,9 +62,12 @@ def factor(n):
     if n == 0 or n == 1:
         factors.append(n)
     else:
-        p_n = primes(math.ceil(math.sqrt(n)))
+        limit = _limit(n)
 
-        for p in p_n:
+        if len(prime_numbers) == 0 or prime_numbers[-1] < limit:
+            prime_numbers = _generate_primes(limit + 1, prime_numbers)
+
+        for p in prime_numbers:
             while True:
                 if n % p == 0:
                     factors.append(p)
